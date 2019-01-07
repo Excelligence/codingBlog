2022d0d2f605362db0b7f8b2cd8d5f1762a5337c
excelligence <excelligence.systems@gmail.com>
2019-01-07 22:58:12 +0530

    added home page, customised bootstrap variable

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
-
-
-gulp.task('browser-sync', function() {
-    browserSync.init({
-      
-        proxy: "localhost:4000"
-        
+try{
+    gulp.task('browser-sync', function() {
+        browserSync.init({
+          
+            proxy: "localhost:4000"
+            
+        });
+        gulp.watch(['_site/*.*','_drafts/*.*','assets/*.*']).on('change',browserSync.reload)
     });
-    gulp.watch(['_site/*.*','_drafts/*.*','assets/*.*']).on('change',browserSync.reload)
-});
+    
+ 
 
+  
+
+
+
+
+
+
+gulp.task('default', gulp.parallel('browser-sync'));
+}
+
+catch(err){
+    console.log(err)
+}
